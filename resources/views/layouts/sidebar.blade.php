8db223db4d87193e81270820bbc727c1beefea06
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-03 13:23:16 +0800

    feat: separate sidebar and customize sidebar menu

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,190 @@
+<div class="sidebar-menu">
+  <ul class="menu">
+    <li class="sidebar-title">Menu</li>
+
+    <li class="sidebar-item active">
+      <a href="index.html" class="sidebar-link">
+        <i class="bi bi-grid-fill"></i>
+        <span>Dashboard</span>
+      </a>
+    </li>
+
+    {{-- <li class="sidebar-item has-sub">
+      <a href="#" class="sidebar-link">
+        <i class="bi bi-stack"></i>
+        <span>Components</span>
+      </a>
+      <ul class="submenu">
+        <li class="submenu-item">
+          <a href="component-alert.html">Alert</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-badge.html">Badge</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-breadcrumb.html">Breadcrumb</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-button.html">Button</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-card.html">Card</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-carousel.html">Carousel</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-collapse.html">Collapse</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-dropdown.html">Dropdown</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-list-group.html">List Group</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-modal.html">Modal</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-navs.html">Navs</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-pagination.html">Pagination</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-progress.html">Progress</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-spinner.html">Spinner</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-toasts.html">Toasts</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-tooltip.html">Tooltip</a>
+        </li>
+      </ul>
+    </li> --}}
+
+    {{-- <li class="sidebar-item has-sub">
+      <a href="#" class="sidebar-link">
+        <i class="bi bi-collection-fill"></i>
+        <span>Extra Components</span>
+      </a>
+      <ul class="submenu">
+        <li class="submenu-item">
+          <a href="extra-component-avatar.html">Avatar</a>
+        </li>
+        <li class="submenu-item">
+          <a href="extra-component-sweetalert.html">Sweet Alert</a>
+        </li>
+        <li class="submenu-item">
+          <a href="extra-component-toastify.html">Toastify</a>
+        </li>
+        <li class="submenu-item">
+          <a href="extra-component-rating.html">Rating</a>
+        </li>
+        <li class="submenu-item">
+          <a href="extra-component-divider.html">Divider</a>
+        </li>
+      </ul>
+    </li> --}}
+
+    {{-- <li class="sidebar-item has-sub">
+      <a href="#" class="sidebar-link">
+        <i class="bi bi-grid-1x2-fill"></i>
+        <span>Layouts</span>
+      </a>
+      <ul class="submenu">
+        <li class="submenu-item">
+          <a href="layout-default.html">Default Layout</a>
+        </li>
+        <li class="submenu-item">
+          <a href="layout-vertical-1-column.html">1 Column</a>
+        </li>
+        <li class="submenu-item">
+          <a href="layout-vertical-navbar.html">Vertical Navbar</a>
+        </li>
+        <li class="submenu-item">
+          <a href="layout-rtl.html">RTL Layout</a>
+        </li>
+        <li class="submenu-item">
+          <a href="layout-horizontal.html">Horizontal Menu</a>
+        </li>
+      </ul>
+    </li> --}}
+
+    <li class="sidebar-title">Data Master</li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Komoditas</span>
+      </a>
+    </li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Program Studi</span>
+      </a>
+    </li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Kelas</span>
+      </a>
+    </li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Mata Kuliah</span>
+      </a>
+    </li>
+
+    <li class="sidebar-item has-sub">
+      <a href="#" class="sidebar-link">
+        <i class="bi bi-stack"></i>
+        <span>Peminjaman</span>
+      </a>
+      <ul class="submenu">
+        <li class="submenu-item">
+          <a href="component-alert.html">Peminjaman Hari Ini</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-badge.html">Histori Peminjaman</a>
+        </li>
+        <li class="submenu-item">
+          <a href="component-badge.html">Laporan</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="sidebar-title">Manajemen Akun</li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Mahasiswa</span>
+      </a>
+    </li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Administrator</span>
+      </a>
+    </li>
+
+    <li class="sidebar-title"></li>
+
+    <li class="sidebar-item">
+      <a href="form-layout.html" class="sidebar-link">
+        <i class="bi bi-file-earmark-medical-fill"></i>
+        <span>Logout</span>
+      </a>
+    </li>
+  </ul>
+</div>
