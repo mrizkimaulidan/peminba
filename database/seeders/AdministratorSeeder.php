65f2a519c090ba552ec2b1a66b43335eed2a4967
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-03 08:18:33 +0800

    feat: add seeder for administrator

---
+++ database/seeders/AdministratorSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Administrator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Administrator::create([
+            'name' => 'Administrator',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('secret'),
+            'phone_number' => fake()->phoneNumber()
+        ]);
+    }
+}
