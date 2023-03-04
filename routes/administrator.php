415f528cacb2d45997701c200b03fa1091eb10f8
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-04 13:48:15 +0800

    feat: controller commodity index

--- routes/administrator.php
+++ routes/administrator.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Http\Controllers\Administrator\CommodityController;
 use App\Http\Controllers\Administrator\DashboardController;
 use App\Http\Controllers\Authentication\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('administrators.')->prefix('administrator')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
-    Route::get('/commodities', function () {
-        return view('administrator.commodity.index');
-    });
+    Route::get('/commodities', [CommodityController::class, 'index'])->name('commodities.index');
 });
