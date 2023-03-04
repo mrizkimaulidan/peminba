bcf8728d7e58b2ff2ebe651730f33b6a2c3bdcde
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-04 13:52:16 +0800

    feat: dashboard URL

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <li class="sidebar-title">Menu</li>
 
     <li class="sidebar-item {{ request()->routeIs('administrators.dashboard') ? 'active' : '' }}">
-      <a href="index.html" class="sidebar-link">
+      <a href="{{ route('administrators.dashboard') }}" class="sidebar-link">
         <i class="bi bi-grid-fill"></i>
         <span>Dashboard</span>
       </a>
