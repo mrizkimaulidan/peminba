e834a1d368b687223fe41ec36c881589f9d9e19c
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-04 14:32:47 +0800

    feat: middleware auth for administrator

--- routes/administrator.php
+++ routes/administrator.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Administrator\DashboardController;
 use App\Http\Controllers\Authentication\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('administrators.')->prefix('administrator')->group(function () {
+Route::middleware('auth:administrator')->name('administrators.')->prefix('administrator')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
     Route::get('/commodities', [CommodityController::class, 'index'])->name('commodities.index');
 });
