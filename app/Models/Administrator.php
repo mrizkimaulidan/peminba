7be12fff79e447b5ca9430c5ee23d97798719ab4
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-08 11:44:10 +0800

    feat: administrator soft deletes

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Administrator extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'email', 'password', 'phone_number'];
 }
