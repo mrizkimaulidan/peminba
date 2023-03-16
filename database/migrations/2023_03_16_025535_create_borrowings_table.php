f6820b31b525debbbb9c7b7e2e3102b039ef2e0b
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-16 11:01:31 +0800

    menambahkan model dan migrasi

---
+++ database/migrations/2023_03_16_025535_create_borrowings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrowings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('commodity_id')->constrained();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->date('date');
+            $table->time('time_start');
+            $table->time('time_end')->nullable();
+            $table->boolean('is_returned')->default(0);
+            $table->text('note')->nullable();
+            $table->string('officer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrowings');
+    }
+};
