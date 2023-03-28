9a3beb75f3cab18caadf8dcd028c0627588e9980
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-03-29 02:46:37 +0800

    chore: wrap into document onready

--- resources/views/administrator/borrowing/script.blade.php
+++ resources/views/administrator/borrowing/script.blade.php
@@ -1,4 +1,6 @@
 <script>
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  $(function() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+  });
 </script>
