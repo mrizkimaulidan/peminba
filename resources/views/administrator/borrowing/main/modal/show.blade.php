e93aa3d358af93fad793498f72fdfd94bdab53a9
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-04-02 20:19:05 +0800

    style: :art: remove update button

--- resources/views/administrator/borrowing/main/modal/show.blade.php
+++ resources/views/administrator/borrowing/main/modal/show.blade.php
@@ -106,7 +106,6 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary close-button" data-bs-dismiss="modal">Tutup</button>
-          <button type="submit" class="btn btn-success">Ubah</button>
         </div>
       </div>
     </div>
