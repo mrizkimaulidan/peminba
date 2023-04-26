2a13cbde1f5666da46d6016d3dd9cf1dedababbf
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-04-26 13:24:20 +0800

    chore: add required tag html

--- resources/views/administrator/student/modal/create.blade.php
+++ resources/views/administrator/student/modal/create.blade.php
@@ -15,8 +15,7 @@
                 <input type="number" name="identification_number"
                   class="form-control @error('identification_number', 'store') is-invalid @enderror"
                   @if($errors->hasBag('store'))
-                value="{{ old('identification_number') }}" @endif placeholder="Masukkan nim.."
-                >
+                value="{{ old('identification_number') }}" @endif placeholder="Masukkan nim.." required>
                 @error('identification_number', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
@@ -29,7 +28,7 @@
                 <label for="name" class="form-label">Nama</label>
                 <input type="text" name="name" class="form-control @error('name', 'store') is-invalid @enderror"
                   @if($errors->hasBag('store'))
-                value="{{ old('name') }}" @endif placeholder="Masukkan nama.." >
+                value="{{ old('name') }}" @endif placeholder="Masukkan nama.." required>
                 @error('name', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
@@ -44,7 +43,7 @@
               <div class="mb-3">
                 <label for="program_study_id" class="form-label">Program Studi</label>
                 <select class="form-select @error('program_study_id', 'store') is-invalid @enderror"
-                  name="program_study_id">
+                  name="program_study_id" required>
                   <option value="0">Pilih..</option>
                   @foreach ($programStudies as $programStudy)
                   <option value="{{ $programStudy->id }}" {{ old('program_study_id')===(string)$programStudy->id ?
@@ -64,7 +63,7 @@
               <div class="mb-3">
                 <label for="school_class_id" class="form-label">Kelas</label>
                 <select class="form-select @error('school_class_id', 'store') is-invalid @enderror"
-                  name="school_class_id">
+                  name="school_class_id" required>
                   <option value="0">Pilih..</option>
                   @foreach ($schoolClasses as $schoolClass)
                   <option value="{{ $schoolClass->id }}" {{ old('school_class_id')===(string)$schoolClass->id ?
@@ -87,7 +86,7 @@
                 <span class="d-block input-group-text"><i class="bi bi-envelope-at-fill"></i></span>
                 <input type="email" name="email" class="form-control @error('email', 'store') is-invalid @enderror"
                   @if($errors->hasBag('store'))
-                value="{{ old('email') }}" @endif placeholder="Masukkan email.." >
+                value="{{ old('email') }}" @endif placeholder="Masukkan email.." required>
                 @error('email', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
@@ -103,7 +102,7 @@
                   class="form-control @error('phone_number', 'store') is-invalid @enderror"
                   @if($errors->hasBag('store'))
                 value="{{ old('phone_number') }}" @endif
-                placeholder="Masukkan nomor handphone.." >
+                placeholder="Masukkan nomor handphone.." required>
                 @error('phone_number', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
@@ -118,7 +117,7 @@
                 <label for="password" class="form-label">Password</label>
                 <input type="password" name="password"
                   class="form-control @error('password', 'store') is-invalid @enderror"
-                  placeholder="Masukkan password..">
+                  placeholder="Masukkan password.." required>
                 @error('password', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
@@ -131,7 +130,7 @@
                 <label for="password_confirmation" class="form-label">Konfirmasi Password</label>
                 <input type="password" name="password_confirmation"
                   class="form-control @error('password_confirmation', 'store') is-invalid @enderror"
-                  placeholder="Masukkan password..">
+                  placeholder="Masukkan password.." required>
                 @error('password_confirmation', 'store')
                 <div class="d-block invalid-feedback">
                   {{ $message }}
