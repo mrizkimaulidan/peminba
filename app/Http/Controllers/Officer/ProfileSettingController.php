15f990e637e228baf151149ddbc9d27a6c477131
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-04-26 14:25:15 +0800

    feat: :sparkles: officer update profile validation

--- app/Http/Controllers/Officer/ProfileSettingController.php
+++ app/Http/Controllers/Officer/ProfileSettingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Officer;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileSettingRequest;
 use App\Models\Officer;
 use Illuminate\Http\Request;
 
@@ -19,15 +20,16 @@ class ProfileSettingController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request)
+    public function update(UpdateProfileSettingRequest $request)
     {
         $officer = Officer::find(auth('officer')->id());
+        $validated = $request->validated();
 
         $officer->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone_number' => $request->phone_number,
-            'password' => $request->password ?? $officer->password
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'phone_number' => $validated['phone_number'],
+            'password' => !is_null($validated['password']) ? bcrypt($validated['password']) : $officer->password
         ]);
 
         return redirect()->route('officers.profile-settings.index')->with('success', 'Data berhasil diubah!');
