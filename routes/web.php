d36e05c141d308479b2ce99d6c63583f7452054d
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-05-06 22:34:44 +0800

    chore: auto redirect to login when accessing root URL

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
