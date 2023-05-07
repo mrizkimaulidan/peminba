965dede8a5dc06bc145372aa142d0795e585b579
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2023-05-07 13:10:57 +0800

    chore: required tag on each input

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -24,13 +24,15 @@
           <form action="{{ route('login') }}" method="POST">
             @csrf
             <div class="form-group position-relative has-icon-left mb-4">
-              <input type="email" name="email" class="form-control form-control-xl" placeholder="Email" autofocus />
+              <input type="email" name="email" class="form-control form-control-xl" placeholder="Email" autofocus
+                required />
               <div class="form-control-icon">
                 <i class="bi bi-person"></i>
               </div>
             </div>
             <div class="form-group position-relative has-icon-left mb-4">
-              <input type="password" name="password" class="form-control form-control-xl" placeholder="Password" />
+              <input type="password" name="password" class="form-control form-control-xl" placeholder="Password"
+                required />
               <div class="form-control-icon">
                 <i class="bi bi-shield-lock"></i>
               </div>
